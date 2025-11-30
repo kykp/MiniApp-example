538829853b9b6098ae425fa92008c78ec3d9c57d
konstantin revutsky <kykp@yandex.ru>
2025-11-30 09:10:44 +0400

    - главная страница добавлена кнопка и пример карточки

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,33 @@
-import {Button} from "@/shared/ui/Button/Button.tsx";
+import {useNavigate} from "react-router-dom";
+import {Button} from "@/shared/ui";
+import {ROUTES} from "@/shared/config";
 import styles from './HomePage.module.css';
 
 export const HomePage = () => {
+    const navigate = useNavigate();
+
+    const handleTrack = () => navigate(ROUTES.track)
+
     return (
         <div className={styles.homePage}>
-            <p>Добро пожаловать в мини-приложение Telegram!</p>
             <p>Welcome to mini App Telegram!</p>
 
-            <div className={styles.buttons}>
-                <Button variant="primary">Primary</Button>
-                <Button variant="default">Default</Button>
-                <Button variant="outline">Outline</Button>
-                <Button variant="danger">Danger</Button>
+            <div className={styles.create_button}>
+                <Button variant="primary" onClick={handleTrack}>Создать отслеживание</Button>
+            </div>
+
+            <div className={styles.card}>
+
+                <div className={styles.header}>
+                    <span className={styles.status}>STATUS</span>
+                    <h3 className={styles.title}>Tracking title</h3>
+                </div>
+
+                <div className={styles.content}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, suscipit, totam! Adipisci aliquam
+                    animi aperiam at atque aut beatae consequuntur deleniti deserunt doloribus eum eveniet facere fugit,
+                    id in nam nihil.
+                </div>
             </div>
         </div>
     );
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export {Header} from './Header'
 export {PageContent} from './PageContent'
 export {PageWrapper} from './PageWrapper'
+export {Button} from './Button/Button.tsx'
